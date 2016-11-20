14d14127bdf3e9273ced95a7e267c6ec3a309a21
Simon Chiang <simon.a.chiang@gmail.com>
2016-11-19 21:02:58 -0700

    Add linecook formula

---
+++ Formula/linecook.rb
@@ -0,0 +1,20 @@
+require "formula"
+
+class Linecook < Formula
+  homepage "https://github.com/goodlittlescript/linecook"
+  url "https://github.com/goodlittlescript/linecook/archive/v1.0.0.tar.gz"
+  sha256 "db36c28e6e19821784724d378474c1bbe002bd818f96ea936623fe26d76c250c"
+  version "1.0.0"
+
+  def install
+    FileUtils.mkdir_p bin
+    FileUtils.cp "bin/linecook", bin
+
+    FileUtils.mkdir_p man1
+    FileUtils.cp "man/man1/linecook.1", man1
+  end
+
+  test do
+    system "#{bin}/linecook -e \"#{bin}/linecook -X\""
+  end
+end
